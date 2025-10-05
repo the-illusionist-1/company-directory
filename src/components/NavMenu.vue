5e2d0949798625522101429f1ca00543292e53c7
Andres Flores <and05flo@proton.me>
2025-10-05 12:16:34 -0500

    Added login feature

--- src/components/NavMenu.vue
+++ src/components/NavMenu.vue
@@ -1,9 +1,9 @@
 <template>
   <nav class="bg-slate-900 h-20 items-center text-white flex justify-center space-x-4"> 
-    <RouterLink :to="{name: Home}">Home</RouterLink>
-    <RouterLink :to="{name: Other}">Other</RouterLink>
-    <RouterLink :to="{name: LoginPage}">Login</RouterLink>
-    <RouterLink :to="{name: Settings}">Settings</RouterLink>
+    <RouterLink :to="{name: 'Home'}">Home</RouterLink>
+    <RouterLink :to="{name: 'Other'}">Other</RouterLink>
+    <RouterLink :to="{name: 'LoginPage'}">Login</RouterLink>
+    <RouterLink :to="{name: 'SettingsPage'}">Settings</RouterLink>
   </nav>
 </template>
 
--- src/views/LoginPage.vue
+++ src/views/LoginPage.vue
@@ -1,3 +1,38 @@
 <template>
-  Login
+  <form class="" @submit.prevent>
+    <div>
+      <label for="username">Username</label>
+      <input id="username" v-model="username" type="text" placeholder="Username">
+    </div>
+    <div>
+      <label for="password" class="">Password</label>
+      <input id="password" v-model="password" type="password" placeholder="Password">
+    </div>
+    
+    <button @click="logUserin" class="">LOGIN</button>
+  </form>
 </template>
+
+<script setup>
+import  {ref} from 'vue';
+import { useRoute, useRouter } from 'vue-router';
+import {useAuth} from '../composables/useAuth.js'
+
+const {login, logout} = useAuth();
+const router = useRoute();
+
+const username = ref('');
+const password = ref('');
+
+const logUserin = async () => {
+  if (await login(username.value, password.value)) {
+    if (route.query.redirect) {
+      router.push(route.query.redirect);
+    } else {
+      router.push({name: 'SettingsPage'})
+    }
+  } else {
+    logout()
+  }
+}
+</script>
